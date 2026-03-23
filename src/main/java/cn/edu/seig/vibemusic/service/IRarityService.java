6f62a54bc17304104b66732315e0fb064a83977b
lilac-kk <184565509@qq.com>
2026-03-23 10:59:28 +0800

    Create IRarityService.java

---
+++ src/main/java/cn/edu/seig/vibemusic/service/IRarityService.java
@@ -0,0 +1,7 @@
+package cn.edu.seig.vibemusic.service;
+
+import cn.edu.seig.vibemusic.model.entity.Rarity;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+public interface IRarityService extends IService<Rarity> {
+}
