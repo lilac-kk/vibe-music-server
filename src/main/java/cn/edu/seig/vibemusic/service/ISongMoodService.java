2167c42e3504b2654e26df16efaaee56a29da93a
lilac-kk <184565509@qq.com>
2026-03-23 11:04:17 +0800

    Create ISongMoodService interface for SongMood entity

---
+++ src/main/java/cn/edu/seig/vibemusic/service/ISongMoodService.java
@@ -0,0 +1,7 @@
+package cn.edu.seig.vibemusic.service;
+
+import cn.edu.seig.vibemusic.model.entity.SongMood;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+public interface ISongMoodService extends IService<SongMood> {
+}
