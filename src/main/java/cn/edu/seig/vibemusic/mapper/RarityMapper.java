ce7dbcc80fe40dca024488cc1f74c8d473f9f050
lilac-kk <184565509@qq.com>
2026-03-23 10:42:04 +0800

    Create RarityMapper interface for Rarity entity

---
+++ src/main/java/cn/edu/seig/vibemusic/mapper/RarityMapper.java
@@ -0,0 +1,12 @@
+package cn.edu.seig.vibemusic.mapper;
+ 
+import cn.edu.seig.vibemusic.model.entity.Rarity;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
+ 
+/**
+ * 稀有度Mapper
+ */
+@Mapper
+public interface RarityMapper extends BaseMapper<Rarity> {
+}
