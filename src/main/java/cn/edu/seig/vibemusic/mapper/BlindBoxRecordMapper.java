a850914c3105a36667223fe8597248f8751aeee5
lilac-kk <184565509@qq.com>
2026-03-23 10:50:41 +0800

    Create BlindBoxRecordMapper interface

---
+++ src/main/java/cn/edu/seig/vibemusic/mapper/BlindBoxRecordMapper.java
@@ -0,0 +1,12 @@
+package cn.edu.seig.vibemusic.mapper;
+ 
+import cn.edu.seig.vibemusic.model.entity.BlindBoxRecord;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
+ 
+/**
+ * 盲盒抽取记录Mapper
+ */
+@Mapper
+public interface BlindBoxRecordMapper extends BaseMapper<BlindBoxRecord> {
+}
