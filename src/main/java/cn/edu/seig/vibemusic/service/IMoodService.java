c5e2ea33d6e36a6d79b86fc388a490982fde996c
lilac-kk <184565509@qq.com>
2026-03-23 10:56:19 +0800

    Create IMoodService.java

---
+++ src/main/java/cn/edu/seig/vibemusic/service/IMoodService.java
@@ -0,0 +1,7 @@
+package cn.edu.seig.vibemusic.service;
+
+import cn.edu.seig.vibemusic.model.entity.Mood;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+public interface IMoodService extends IService<Mood> {
+}
