9d32af7e83862399bafd7d709557fcd6ed277489
lilac-kk <184565509@qq.com>
2026-03-23 09:49:38 +0800

    Create Rarity.java

---
+++ src/main/java/cn/edu/seig/vibemusic/model/entity/Rarity.java
@@ -0,0 +1,47 @@
+package cn.edu.seig.vibemusic.model.entity;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+/**
+ * 歌曲稀有度实体
+ */
+@Data
+@EqualsAndHashCode(callSuper = false)
+@TableName("tb_rarity")
+public class Rarity implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    @TableId(value = "id", type = IdType.AUTO)
+    private Long id;
+
+    @TableField("name")
+    private String name;
+
+    @TableField("weight")
+    private Integer weight;
+
+    @TableField("color")
+    private String color;
+
+    @TableField("icon")
+    private String icon;
+
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    @TableField("create_time")
+    private LocalDateTime createTime;
+
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    @TableField("update_time")
+    private LocalDateTime updateTime;
+}
