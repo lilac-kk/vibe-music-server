e1d28a80af06180f9e5f8c6e50bec78663fb1e26
lilac-kk <184565509@qq.com>
2026-03-23 11:05:14 +0800

    Create IArtistMoodService interface for ArtistMood

---
+++ src/main/java/cn/edu/seig/vibemusic/service/IArtistMoodService.java
@@ -0,0 +1,7 @@
+package cn.edu.seig.vibemusic.service;
+
+import cn.edu.seig.vibemusic.model.entity.ArtistMood;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+public interface IArtistMoodService extends IService<ArtistMood> {
+}
