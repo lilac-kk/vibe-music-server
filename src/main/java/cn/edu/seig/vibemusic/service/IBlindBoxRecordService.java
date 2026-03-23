cc195b7ee4a3c83059628645db218ab980736a32
lilac-kk <184565509@qq.com>
2026-03-23 11:06:58 +0800

    Create IBlindBoxRecordService interface

---
+++ src/main/java/cn/edu/seig/vibemusic/service/IBlindBoxRecordService.java
@@ -0,0 +1,7 @@
+package cn.edu.seig.vibemusic.service;
+
+import cn.edu.seig.vibemusic.model.entity.BlindBoxRecord;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+public interface IBlindBoxRecordService extends IService<BlindBoxRecord> {
+}
