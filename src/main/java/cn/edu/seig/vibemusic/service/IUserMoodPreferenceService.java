3679b986fa641e7e081a574e0d828c469ad897f2
lilac-kk <184565509@qq.com>
2026-03-23 11:02:35 +0800

    Add IUserMoodPreferenceService interface

---
+++ src/main/java/cn/edu/seig/vibemusic/service/IUserMoodPreferenceService.java
@@ -0,0 +1,7 @@
+package cn.edu.seig.vibemusic.service;
+
+import cn.edu.seig.vibemusic.model.entity.UserMoodPreference;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+public interface IUserMoodPreferenceService extends IService<UserMoodPreference> {
+}
