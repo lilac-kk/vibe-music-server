1734a792d0fb229f02ec4ba636b253dca87823a9
lilac-kk <184565509@qq.com>
2026-03-23 11:17:56 +0800

    Implement BlindBoxRecordServiceImpl class

---
+++ src/main/java/cn/edu/seig/vibemusic/service/impl/BlindBoxRecordServiceImpl.java
@@ -0,0 +1,11 @@
+package cn.edu.seig.vibemusic.service.impl;
+
+import cn.edu.seig.vibemusic.mapper.BlindBoxRecordMapper;
+import cn.edu.seig.vibemusic.model.entity.BlindBoxRecord;
+import cn.edu.seig.vibemusic.service.IBlindBoxRecordService;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.stereotype.Service;
+
+@Service
+public class BlindBoxRecordServiceImpl extends ServiceImpl<BlindBoxRecordMapper, BlindBoxRecord> implements IBlindBoxRecordService {
+}
