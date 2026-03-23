3f1d73e8d3e80578722a792af38855f3bc17f78e
lilac-kk <184565509@qq.com>
2026-03-23 10:50:58 +0800

    Add BlindBoxConfigMapper interface

---
+++ src/main/java/cn/edu/seig/vibemusic/mapper/BlindBoxConfigMapper.java
@@ -0,0 +1,12 @@
+package cn.edu.seig.vibemusic.mapper;
+ 
+import cn.edu.seig.vibemusic.model.entity.BlindBoxConfig;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
+ 
+/**
+ * 盲盒配置Mapper
+ */
+@Mapper
+public interface BlindBoxConfigMapper extends BaseMapper<BlindBoxConfig> {
+}
