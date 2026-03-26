223f9bc3f3ba9703beb5ac71ae279d8c46918c65
lilac-kk <184565509@qq.com>
2026-03-26 16:16:27 +0800

    Create BlindBoxMessageConstant.java

---
+++ src/main/java/cn/edu/seig/vibemusic/constant/BlindBoxMessageConstant.java
@@ -0,0 +1,29 @@
+package cn.edu.seig.vibemusic.constant;
+ 
+/**
+ * 盲盒功能消息常量
+ */
+public class BlindBoxMessageConstant {
+    
+    public static final String BLIND_BOX = "盲盒";
+    
+    public static final String DRAW_SUCCESS = "抽取成功";
+    
+    public static final String DRAW_FAILED = "抽取失败";
+    
+    public static final String FEATURE_DISABLED = "功能未启用";
+    
+    public static final String DAILY_LIMIT = "每日抽取次数";
+    
+    public static final String REMAINING_DRAWS = "剩余抽取次数";
+    
+    public static final String INVALID_MOOD = "无效的情绪";
+    
+    public static final String EMPTY_POOL = "候选池为空";
+    
+    public static final String FEATURE_ENABLED = "功能已启用";
+    
+    public static final String FEATURE_DISABLED = "功能已禁用";
+    
+    public static final String USER_HISTORY = "抽取历史";
+}
